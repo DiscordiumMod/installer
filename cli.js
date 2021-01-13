e5748c0ad12fe45893d0ff64935c3a183095b913
ItsRauf <raufislam2003@gmail.com>
2021-01-13 18:46:04 -0500

    patch and unpatch

--- cli.js
+++ cli.js
@@ -1,1 +1,35 @@
 #!/usr/bin/env node
+const yargs = require("yargs/yargs");
+const { hideBin } = require("yargs/helpers");
+
+const cli = yargs(hideBin(process.argv));
+
+cli
+  .command(
+    "patch [discordVersion]",
+    "Patches Discordium into Discord.",
+    (argv) => {
+      argv.positional("discordVersion", {
+        describe: "Discord version to patch.",
+        default: "DiscordCanary",
+      });
+    },
+    (args) => {
+      const patch = require("./lib/patch");
+      patch(args);
+    }
+  )
+  .command(
+    "unpatch [discordVersion]",
+    "Unpatches Discordium into Discord.",
+    (argv) => {
+      argv.positional("discordVersion", {
+        describe: "Discord version to unpatch.",
+        default: "canary",
+      });
+    },
+    (args) => {
+      const unpatch = require("./lib/unpatch");
+      unpatch(args);
+    }
+  ).argv;
